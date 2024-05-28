1a99d72e546ab083c797c7653208813e47c52d38
Thomas-Stoianov <thomasgstoianov2@gmail.com>
2024-05-28 17:22:45 -0300

    Feat: sons colocados no jogo

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Actor, CollisionType, Color, Engine, Font, Text, vec } from "excalibur"
+import { Actor, CollisionType, Color, Engine, Font, FontUnit, Label, Loader, Sound, Text, vec } from "excalibur"
 
 // 1 - criar uma instância de Engine, que representa o jogo
 const game = new Engine({
@@ -123,21 +123,38 @@ listaBlocos.forEach( bloco => {
 // Adicionando pontuação
 let pontos = 0
 
-const textoPontos = new Text({
-	text: "Hello World",
-	font: new Font({size: 20})
-})
-const objetoTexto = new Actor({
-	x: game.drawWidth - 50,
-	y: game.drawHeight - 50,
+const textoPontos = new Label({
+	text: pontos.toString(),
+	font: new Font({
+		size: 40,
+		color: Color.White,
+		strokeColor: Color.Black,
+		unit: FontUnit.Px
+	}),
+	pos: vec(600, 500)
 })
 
-objetoTexto.graphics.use(textoPontos)
+game.add(textoPontos)
 
-game.add(objetoTexto)
+
+// const textoPontos = new Text({
+// 	text: "Hello World",
+// 	font: new Font({size: 20})
+// })
+// const objetoTexto = new Actor({
+// 	x: game.drawWidth - 50,
+// 	y: game.drawHeight - 50,
+// })
+
+// objetoTexto.graphics.use(textoPontos)
+
+// game.add(objetoTexto)
 
 let colidindo: boolean = false
 
+const sound = new Sound("./audio.mp3");
+const sound2 = new Sound("./audioMetal.mp3");
+const loader = new Loader([sound, sound2]);
 
 bolinha.on("collisionstart", (event) => {
 
@@ -150,7 +167,16 @@ bolinha.on("collisionstart", (event) => {
 
 		// Destruir o bloco colidido
 		event.other.kill()
+
+		// Adiciona um ponto
+		pontos++
+
+		// Atualiza o valor do placar - textoPontos
+		textoPontos.text = pontos.toString()
+
+		sound.play();
 	}
+
 
 	// Rebater a bolinha = inverter as direções x e y
 	// minimum trasnlation vector
@@ -175,14 +201,16 @@ bolinha.on("collisionstart", (event) => {
 	}
 })
 
+
 bolinha.on("collisionend", () => {
 	colidindo = false
 })
 
 bolinha.on("exitviewport", () => {
+	sound2.play()
 	alert("E morreu")
 	window.location.reload()
 })
 
 // Inicia o game
-game.start()
+game.start(loader)
